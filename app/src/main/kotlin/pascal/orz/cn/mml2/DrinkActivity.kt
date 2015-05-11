21c45593d15bef1811b6f1f220ade88dbeb6cfd2
koduki <koduki@nebula>
2015-05-12 06:45:17 +0900

    add simple menu.

--- app/src/main/kotlin/pascal/orz/cn/mml2/DrinkActivity.kt
+++ app/src/main/kotlin/pascal/orz/cn/mml2/DrinkActivity.kt
@@ -5,8 +5,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ListView
+import android.widget.TextView
 
 
 public class DrinkActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drink)
+
+        val intent = getIntent()
+        val drinkName = intent.getSerializableExtra("drink_name") as String
+        val drinkNameView = findViewById(R.id.drink_name) as (TextView)
+        drinkNameView.setText(drinkName)
+    }
 }
--- app/src/main/kotlin/pascal/orz/cn/mml2/MainActivity.kt
+++ app/src/main/kotlin/pascal/orz/cn/mml2/MainActivity.kt
@@ -1,9 +1,13 @@
 package pascal.orz.cn.mml2
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 
 
@@ -14,11 +18,24 @@ public class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val drinkAdapter = DrinkAdapter(this)
-        val drinkList =  findViewById(R.id.drinkListView) as (ListView)
+        val drinkList = findViewById(R.id.drinkListView) as (ListView)
         drinkList.setAdapter(drinkAdapter)
         drinkAdapter.add("日本酒")
         drinkAdapter.add("ビール")
         drinkAdapter.add("ワイン")
+
+        drinkList.setOnItemClickListener(object : AdapterView.OnItemClickListener {
+            override fun onItemClick(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
+                //def list = (ListView) findViewById(R.id.part_list)
+                //def part = (Part) list.getItemAtPosition(position)
+                val name = adapterView.getItemAtPosition(i) as String
+                        Log.i("hogehoge", name)
+                 val intent = Intent(getApplicationContext(), javaClass<DrinkActivity>())
+                intent.putExtra("drink_name", name);
+
+                startActivity(intent)
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
