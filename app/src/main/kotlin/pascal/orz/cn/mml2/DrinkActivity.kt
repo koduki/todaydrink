e73e86cbec7030c45230b50e7eb04daed0b03a5d
koduki <koduki@nebula>
2015-05-13 08:18:13 +0900

    add firebase.

--- app/src/main/kotlin/pascal/orz/cn/mml2/DrinkActivity.kt
+++ app/src/main/kotlin/pascal/orz/cn/mml2/DrinkActivity.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import com.firebase.client.Firebase
 import kotlinx.android.synthetic.activity_drink.*
 
 public class DrinkActivity : AppCompatActivity() {
@@ -14,12 +15,17 @@ public class DrinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink)
+        Firebase.setAndroidContext(this);
+
 
         val intent = getIntent()
         val name = intent.getSerializableExtra("drink_name") as String
         drink_name.setText(name)
 
         drunk_button.setOnClickListener {
+            val firebase = Firebase("https://shining-heat-6127.firebaseio.com/")
+            firebase.child("data").child("item").setValue("Hello World.")
+
             Toast.makeText(getApplication(), "ドリンクヒストリーに追加しました", Toast.LENGTH_LONG)?.show()
         }
     }
